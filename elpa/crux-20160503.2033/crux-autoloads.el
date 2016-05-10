c59b2d761ef9a8417f885f6a45f7640c50ca3a9b
shen <shenxuhui1992@gmail.com>
2016-05-10 15:29:50 +0800

    Wtf crux?

--- elpa/crux-20160503.2033/crux-autoloads.el
+++ elpa/crux-20160503.2033/crux-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "crux" "crux.el" (22321 34733 0 0))
+;;;### (autoloads nil "crux" "crux.el" (22321 36159 0 0))
 ;;; Generated autoloads from crux.el
 
 (defvar crux-reopen-as-root-mode nil "\
